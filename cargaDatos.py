28d5091c7d2cb67b47063f9aad58977f11ab96d9
VAlexS <vsanzc@alumnos.nebrija.es>
2024-03-24 22:53:26 +0100

    cambios 24 marzo

--- cargaDatos.py
+++ cargaDatos.py
@@ -21,20 +21,20 @@ print(df_cleanded.columns)
 print(df_cleanded.head(n=20))
 
 #dado que a simple vista se ve que la columna de los likes tiene todos 0, realizo esta comprobacion
-likes_cero = (df['Likes'] == 0).all()
+likes_cero = (df_cleanded['Likes'] == 0).all()
 
 if likes_cero:
     print("Todos los valores de la columna <<Likes>> son 0")
 else:
-    num_valores_no_cero = (df['Likes'] != 0).sum()
+    num_valores_no_cero = (df_cleanded['Likes'] != 0).sum()
     print("Cantidad de valores en la columna <<Likes>> distinta de 0: ",num_valores_no_cero)
     
 
 #calculo el porcentaje de valores faltantes
-num_valores_totales = df.size
+num_valores_totales = df_cleanded.size
 
 # Calcular el número total de valores no nulos en el DataFrame
-num_valores_no_nulos = df.count().sum()
+num_valores_no_nulos = df_cleanded.count().sum()
 
 # Calcular el número total de valores faltantes en el DataFrame
 num_valores_faltantes = num_valores_totales - num_valores_no_nulos
@@ -45,22 +45,10 @@ porcentaje_valores_faltantes = (num_valores_faltantes / num_valores_totales) * 1
 
 print("Porcentaje de valores faltantes en el DataFrame:", porcentaje_valores_faltantes * 100,"%")
 
-'''
-#elimino filas con valores faltantes
-df_cleanded.dropna(inplace=True)
 
-print("COLUMNAS (con las variables de interes)")
-print(df_cleanded.columns)
 
-print("_____________________")
-
-print("PARTE DEL FICHERO")
-
-print(df_cleanded.head(n=10))
-
-#ya que tenemos el dataframe sin valores faltantes, genero un nuevo .csv con ese dataframe
 
 df_cleanded.to_csv('files\TwitterDatashetClean.csv', index=False)
-'''
+
 
 
--- transformacion.py
+++ transformacion.py
@@ -3,25 +3,46 @@ import pandas as pd
 
 def determinar_dia(weekday):
     if weekday in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']:
-        return 0
+        return 'No'
     else:
-        return 1
+        return 'Yes'
+    
+    
+def imprimir_dataframe(df):
+    print(df.columns)
+    print(df.head(n=20))
+    
+def print_descripcion(df):
+    print(df.describe())
+    
+
+
 
 
 df = pd.read_csv('files\TwitterDatashetClean.csv', low_memory=False)
 
-'''
-print(df.columns)
-
-print(df.head(n=10))
-'''
 
 #la columna weekend toma 2 valores posibles: 0 si es entre semana y 1 si es fin de semana
 df['Weekend'] = df['Weekday'].apply(determinar_dia) 
 
-print(df.columns)
 
-print(df.head(n=70))
+print("_________________________________________________________")
+
+#en función de las franjas horarias, defino el valor correspondiente al momento del dia
+
+franjas_horarias = [0, 3, 7, 13, 22, 24]
+
+momentos_dia = ['Noche', 'Madrugada', 'Mañana', 'Tarde', 'Noche']
+
+df['FranjaHoraria'] = pd.cut(df['Hour'], bins=franjas_horarias, labels=momentos_dia, right=False, ordered=False)
+
+
+imprimir_dataframe(df)
+
+
+
+
+df.to_csv('files\TwitterDatashetCleanB.csv', index=False)
 
 
 
